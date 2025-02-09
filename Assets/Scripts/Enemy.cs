4708bf0dd2b8cf7359601e5fe1c80cbd35740790
TylerSNapier <109185047+tylersnapier@users.noreply.github.com>
2025-02-09 13:08:18 -0500

    Initial commit

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    private float _enemySpeed = 4f;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        transform.Translate(Vector3.down * _enemySpeed * Time.deltaTime);
+
+        if (transform.position.y < -5f)
+        {
+            float randomX = Random.Range(-8f, 8f);
+            transform.position = new Vector3(randomX, 7, 0);
+        }
+        //move down at 4 meters per second
+        //if at bottom of screen
+        //respawn at top (with new random X pos
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //if other is player
+        //Damage the player
+        //Destroy enemy
+        if (other.tag == "Player")
+        {
+            Destroy(this.gameObject);
+        }
+
+        //if other is laser
+        //Destroy laser
+        //Destroy enemy
+        if (other.tag == "Laser")
+        {
+            Destroy(other.gameObject);
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+
+    [SerializeField]
+    private float _laserSpeed = 8f;
+
+    
+    void Update()
+    {
+        transform.Translate(Vector3.up * _laserSpeed * Time.deltaTime);
+
+        if (transform.position.y > 8f)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
