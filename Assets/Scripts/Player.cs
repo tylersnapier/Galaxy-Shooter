5b123d980b6a6de79adb2f7932988326d305bb77
TylerSNapier <109185047+tylersnapier@users.noreply.github.com>
2025-02-22 22:08:22 -0500

    Added 2D sprites

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,12 +5,12 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField]
-    private float _speed = 3.5f;
+    private float _speed = 5f;
 
     [SerializeField]
     private GameObject _laserPrefab;
     [SerializeField]
-    private float _fireRate = 1f;
+    private float _fireRate = 0.15f;
     private float _canFire = 0f;
     [SerializeField]
     private int _lives = 3;
@@ -60,7 +60,7 @@ public class Player : MonoBehaviour
     void FireLaser()
     {
         _canFire = Time.time + _fireRate;
-        Instantiate(_laserPrefab, transform.position + new Vector3(0, 0.8f, 0), Quaternion.identity);
+        Instantiate(_laserPrefab, transform.position + new Vector3(0, 1.05f, 0), Quaternion.identity);
     }
 
     public void Damage()
