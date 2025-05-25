b569cb600b36dbf8497e9cc51d25cce9b7734132
TylerSNapier <109185047+tylersnapier@users.noreply.github.com>
2025-05-25 18:22:54 -0400

    Added GameManager and Restart
    Player can now restart after losing all 3 lives

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -14,11 +14,21 @@ public class UIManager : MonoBehaviour
     private Sprite[] _livesSprites;
     [SerializeField]
     private TMP_Text _gameOverText;
+    [SerializeField]
+    private TMP_Text _restartText;
 
+    private GameManager _gameManager;
     void Start()
     {
         _scoreText.text = "Score: " + 0;
         _gameOverText.gameObject.SetActive(false);
+        _gameManager = GameObject.Find("Game_Manager").GetComponent<GameManager>();
+
+        if (_gameManager == null )
+        {
+            Debug.LogError("Game Manager is NULL.");
+        }
+
     }
 
     public void UpdateScore(int playerScore)
@@ -34,9 +44,29 @@ public class UIManager : MonoBehaviour
 
         if(currentLives == 0)
         {
-            _gameOverText.gameObject.SetActive(true);
+
+            GameOverSequence();
         }
     }
 
-    
+    void GameOverSequence()
+    {
+        _gameManager.GameOver();
+        _gameOverText.gameObject.SetActive(true);
+        _restartText.gameObject.SetActive(true);
+        StartCoroutine(GameOverFlickerRoutine());
+    }
+
+    IEnumerator GameOverFlickerRoutine()
+    {
+        while(true)
+        {
+            _gameOverText.text = "Game Over";
+            yield return new WaitForSeconds(0.5f);
+            _gameOverText.text = "";
+            yield return new WaitForSeconds(0.5f);
+
+             
+        }
+    }
 }
