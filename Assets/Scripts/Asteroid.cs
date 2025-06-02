bc0a43238c923a72b1f0f62af619fdb3a2dfaecf
TylerSNapier <tylersnapier@gmail.com>
2025-06-02 05:28:13 -0400

    Added asteroid and asteroid explosion

---
+++ Assets/Scripts/Asteroid.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+    [SerializeField]
+    private float _rotationSpeed = 3.0f;
+
+    [SerializeField]
+    private GameObject _explosionPrefab;
+    
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        //rotate on zed axis
+        transform.Rotate(Vector3.forward * _rotationSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        //check if the collider is a laser
+        if (other.tag == "Laser")
+        {
+            Instantiate(_explosionPrefab, transform.position, Quaternion.identity);
+            Destroy(other.gameObject); //destroy the laser  
+            Destroy(this.gameObject, 0.25f); //destroy the asteroid
+        }
+    }
+    //check for laser collission (TRIGGER)
+    //instantiate explosion at the position of the asteroid
+    //destroy explosion after 3 seconds
+}
